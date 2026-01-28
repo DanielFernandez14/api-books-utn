ff4493f642f45a1c4177492db66426c77999e7e5
DanoDev <daniel.mfernandez1999@gmail.com>
2026-01-28 13:47:13 -0300

    dotenv

--- clase_14 backend/src/index.ts
+++ clase_14 backend/src/index.ts
@@ -6,8 +6,7 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
-
-const PORT = process.env.PORT
+const PORT = process.env.PORT ? Number(process.env.PORT) : 1235
 
 
 const booksSchema = new Schema({
